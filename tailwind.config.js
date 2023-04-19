15a51387f522bde616fce89b85babb92ac7ac648
erfan578 <er000578@gmail.com>
2023-04-19 16:35:41 +0430

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '1.5rem'
+    },
+    fontFamily: {
+      'yekan': 'Yekan'
+    },
+  },
+}
